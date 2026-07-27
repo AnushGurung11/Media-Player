b93194530d8cc94836bb4b2141e22b3623b83e40
AnushGurung11 <anushgrg15@gmail.com>
2026-07-27 13:42:17 +0545

    fix: datadog config file

--- frontend/datadogRUM-init.js
+++ frontend/datadogRUM-init.js
@@ -5,13 +5,13 @@ datadogRum.init({
     applicationId: 'ed18ff50-c8dd-4c25-8f86-3ca53f7b7bc7',
     clientToken: 'pub3adcdb1ec9c1ef5f2a9c8782738f5be4',
     site: 'datadoghq.com',
-    service: 'vibe mediaplayer',
-    env: 'production',				// e.g. 'prod', 'staging-1', 'dev'
-    version: '1.0',	// e.g. '1.0.0'
-    sessionSampleRate: 100,			// capture 100% of sessions
-    sessionReplaySampleRate: 20,	// capture 20% of sessions with replay
-    trackResources: true,			// Enable Resource tracking
-    trackUserInteractions: true,	// Enable Action tracking
-    trackLongTasks: true,			// Enable Long Tasks tracking
+    service: 'vibe-mediaplayer',
+    env: 'production',
+    version: '1.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackResources: true,
+    trackUserInteractions: true,
+    trackLongTasks: true,
     plugins: [reactPlugin({ router: false })],
 });
